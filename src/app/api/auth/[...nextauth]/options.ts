20ba2efcf2d35e9cbfcb723e468715f615869a73
prateek tiwari <tiwariprateek1976@gmail.com>
2024-06-01 17:54:33 +0530

    authjs

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -9,13 +9,47 @@ export const authOptions : NextAuthOptions = {
         CredentialsProvider({
             id: "credentials",
             name : "credentials",
-            Credentials :{
-                username: { label: "Email", type: "text", placeholder: "jsmith" },
+            credentials :{
+                email: { label: "Email", type: "text", placeholder: "jsmith" },
                 password: { label: "Password", type: "password" }
             },
 
-            async authorize(){
+            async authorize(credentials : any): Promise<any>{
+               
+              await dbconnect();
+
+              try {
+
+           const user = await UserModel.findOne({
+                    $or: [
+                        { email: credentials.identifier.email },
+                        { username: credentials.identifier.username }
+                    ]
+                })
                 
+
+                if (!user) {
+                    throw new Error("No user found")
+                }
+
+                if(!user.isverified){
+                    throw new Error("User not verified")
+                }
+
+             const isPasswordCorrect  = await bcrypt.compare(credentials.password, user.password)
+                     
+             if(!isPasswordCorrect){
+                    throw new Error("Password is incorrect")
+             }
+
+                return { email: user.email, username: user.username, id: user._id }
+
+              } catch (error : any) {
+
+                     throw new Error(error)
+                
+              }
+
             }
         })
     ]
