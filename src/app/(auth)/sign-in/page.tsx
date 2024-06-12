0d448cb4cc134020519211d77733cd4c2c0cf1ff
prateek tiwari <tiwariprateek1976@gmail.com>
2024-06-13 01:58:01 +0530

    use of shadcn

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,21 @@
 "use client"
 
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
+import Link from "next/link"
+import { useState } from "react"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+const Page = () => {
+
+  const [username , setUsername] = useState("")
+  const [usernameMessage ,  setUsernameMessage] = useState("")
+  const [ , setPassword] = useState("")
+  const [ isCheckingUsername , setIsCheckingUsername] = useState(false)
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>Page</div>
   )
 }
+
+export default Page
