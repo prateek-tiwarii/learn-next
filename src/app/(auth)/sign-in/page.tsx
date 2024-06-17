f8c403eaf3adfe217e683617e67411476dbe3969
prateek tiwari <tiwariprateek1976@gmail.com>
2024-06-18 01:08:16 +0530

    sign-up

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -8,7 +8,9 @@ import { useEffect, useState } from "react"
 import { useToast } from "@/components/ui/use-toast"
 import { useRouter } from 'next/navigation'
 import { singnupSchemaValidation } from '@/schemas/signupSchema'
-import axios from "axios"
+import axios, { AxiosError } from "axios"
+import { ApiResponse } from '@/types/apiResponse'
+import { set } from 'mongoose'
 
 
 const Page = () => {
@@ -40,15 +42,51 @@ const Page = () => {
       }
 
       try {
-        await axios.get(`/api/check-username-unique`)
+        const response =  await axios.get(`/api/check-username-unique?username=${debouncedUserName}`)
+        setUsernameMessage(response.data.message)
       } catch (error) {
-        
+        const axiosError = error as AxiosError<ApiResponse>;
+
+        setUsernameMessage(axiosError.response?.data.message??"An error has occured")
+      }
+      finally{
+        setIsCheckingUsername(false)
       }
 
     }
    
+    checkUserNameUnique();
   
   },[debouncedUserName])
+
+  const onSubmit = async(data : z.infer<typeof singnupSchemaValidation >)=>{
+     setIsSubmiting(true)
+
+     try {
+
+      const response = await axios.post("/api/sign-up", data);
+
+      toast({
+        title : "success",
+        description : response.data.message,
+      })
+      router.replace(`/verify/${username}`)
+      setIsSubmiting(false)
+      
+     } catch (error) {
+      console.error("error in signup of user ", error);
+
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message ?? "An error has occured"
+
+      toast({
+        title : "sign-up failed",
+        description : errorMessage,
+        variant : destr
+      })
+
+     }
+  }
 
   return (
     <div>
