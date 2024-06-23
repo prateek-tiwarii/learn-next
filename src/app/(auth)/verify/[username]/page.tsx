d74e19a28c33eba5a18e83d87a246c1dc7069f2e
prateek tiwari <tiwariprateek1976@gmail.com>
2024-06-24 01:00:18 +0530

    verification

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,14 +1,17 @@
+"use client"
+
 import { useToast } from '@/components/ui/use-toast';
-import { useParams } from 'next/navigation';
-import { useRouter } from 'next/router';
+import { useParams, useRouter } from 'next/navigation';
 import React, { use } from 'react'
 import { useForm } from 'react-hook-form';
-import { singnupSchemaValidation } from '@/schemas/signupSchema'
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { verifyMessageSchema } from '@/schemas/verifySchema';
 import axios, { AxiosError } from 'axios';
 import { ApiResponse } from '@/types/apiResponse';
+import { FormItem, FormLabel, FormMessage , Form, FormField} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 const VerifyAccount = () => {
 
@@ -23,7 +26,7 @@ const VerifyAccount = () => {
       const onSubmit = async(data : z.infer<typeof verifyMessageSchema>) =>{
 
         try {
-            const response =  await axios.post(`/api/verify/ `,{
+            const response =  await axios.post(`/api/verify-code/ `,{
                 username : params.username,
                 code : data.code
             });
@@ -60,7 +63,35 @@ const VerifyAccount = () => {
 
 
   return (
-    <div>VerifyAccount</div>
+    <div>
+
+<div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="code"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Verify</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+    </div>
   )
 }
 
