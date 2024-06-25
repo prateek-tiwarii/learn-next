555a8a132b38a6bde4cd5f87983352b3f649438e
prateek tiwari <tiwariprateek1976@gmail.com>
2024-06-25 23:47:13 +0530

    sign-in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,101 +1,122 @@
 "use client"
-import { useDebounceValue } from 'usehooks-ts'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z  from "zod"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import { useToast } from "@/components/ui/use-toast"
-import { useRouter } from 'next/navigation'
-import { singnupSchemaValidation } from '@/schemas/signupSchema'
-import axios, { AxiosError } from "axios"
 import { ApiResponse } from '@/types/apiResponse'
-import { set } from 'mongoose'
+import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDebounceCallback } from 'usehooks-ts';
+import * as z from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
+import axios, { AxiosError } from 'axios';
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { singnupSchemaValidation } from '@/schemas/signupSchema'
+import { signinSchemaValidation } from '@/schemas/signinSchema';
+import { signIn } from 'next-auth/react';
 
 
-const Page = () => {
-
-  const [username , setUsername] = useState("")
-  const [usernameMessage ,  setUsernameMessage] = useState("")
-  const [isSubmiting , setIsSubmiting] = useState(false);
-  const [ isCheckingUsername , setIsCheckingUsername] = useState(false)
-  const { toast } = useToast()
-   const router = useRouter()
-  const debouncedUserName  = useDebounceValue(username, 500)
-
-  const form = useForm ({
-    resolver : zodResolver(singnupSchemaValidation),
-    defaultValues : {
-      username : "",
-      email : "",
-      password : ""
-    }
-  })
-
-  useEffect(()=>{
-    const checkUserNameUnique = async() =>{
-     
-
-      if(debouncedUserName){
-        setIsCheckingUsername(true)
-        setUsernameMessage("")
-      }
-
-      try {
-        const response =  await axios.get(`/api/check-username-unique?username=${debouncedUserName}`)
-        setUsernameMessage(response.data.message)
-      } catch (error) {
-        const axiosError = error as AxiosError<ApiResponse>;
-
-        setUsernameMessage(axiosError.response?.data.message??"An error has occured")
-      }
-      finally{
-        setIsCheckingUsername(false)
-      }
-
-    }
-   
-    checkUserNameUnique();
+const SigninUser = () => {
   
-  },[debouncedUserName])
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+  const { toast } = useToast();
 
-  const onSubmit = async(data : z.infer<typeof singnupSchemaValidation >)=>{
-     setIsSubmiting(true)
+  const form = useForm<z.infer<typeof signinSchemaValidation>>({
+    resolver: zodResolver(signinSchemaValidation),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
 
-     try {
-
-      const response = await axios.post("/api/sign-up", data);
-
-      toast({
-        title : "success",
-        description : response.data.message,
-      })
-      router.replace(`/verify/${username}`)
-      setIsSubmiting(false)
-      
-     } catch (error) {
-      console.error("error in signup of user ", error);
-
-      const axiosError = error as AxiosError<ApiResponse>;
-      let errorMessage = axiosError.response?.data.message ?? "An error has occured"
-
-      toast({
-        title : "sign-up failed",
-        description : errorMessage,
-        variant : "destructive"
-      })
-
-      setIsSubmiting(false)
-
-     }
-  }
+ 
+  const onSubmit = async (data: z.infer<typeof signinSchemaValidation>) => {
+   const result =  await signIn("credentials",{
+    redirect: false,
+    identifier : data.identifier,
+    password : data.password,
+   })
+   if(result?.error){
+    toast({
+      title: "Login failed",
+      description: "Incorrect Username Or Password",
+      variant: "destructive",
+    })
+   }
+   if(result?.url){
+    router.replace("/dashboard")
+   }
+  };
 
   return (
-    <div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join True Feedback
+          </h1>
+          <p className="mb-4">Sign up to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <Input {...field} name="email" />
+                  <p className='text-muted text-gray-400 text-sm'>We will send you a verification code</p>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} name="password" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className='w-full' disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                'Sign Up'
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Already a member?{' '}
+            <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+
 }
 
-export default Page
-
+export default SigninUser
