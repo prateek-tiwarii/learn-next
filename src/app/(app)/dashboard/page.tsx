d30953e5f18a9e783e484bf98a142cfd461fa1f4
prateek tiwari <tiwariprateek1976@gmail.com>
2024-07-01 23:43:37 +0530

    changes

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+"use client"
+
+import { useToast } from "@/components/ui/use-toast";
+import { useState } from "react"
+import { Messages } from "@/models/user";
+import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
+
+  const[messages , setMessages] = useState<Messages[]>([]);
+  const[loading , setIsLoading] = useState(false);
+  const [isSwitchLoading , setIsSwitchLoading] = useState(false);
+  const {toast} = useToast();
+
+  const handelDeleteMessage = (messageID :string) =>{
+     
+     setMessages(messages.filter((message) => message._id !== messageID))
+  }
+
+  const {data:session} =useSession();
+
   return (
     <div>
         dashboard
